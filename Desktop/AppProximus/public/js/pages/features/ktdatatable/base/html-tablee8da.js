4129c6c69e425cc301dbd58f77b0a18b7c8bc974
Thiam-Elhadj-Ibrahim <thiam.ibrahim@hotmail.com>
2020-10-18 20:22:30 +0100

    3 eme Commit

--- Desktop/AppProximus/public/js/pages/features/ktdatatable/base/html-tablee8da.js
+++ Desktop/AppProximus/public/js/pages/features/ktdatatable/base/html-tablee8da.js
@@ -1,15 +1,15 @@
 "use strict";
 // Class definition
 
-var KTDatatableHtmlTableDemo = function() {
-    // Private functions
+var KTDatatableHtmlTableDemo = function () {
+	// Private functions
 
-    // demo initializer
-    var demo = function() {
+	// demo initializer
+	var demo = function () {
 
 		var datatable = $('#kt_datatable').KTDatatable({
 			data: {
-				saveState: {cookie: false},
+				saveState: { cookie: false },
 			},
 			search: {
 				input: $('#kt_datatable_search_query'),
@@ -17,100 +17,55 @@ var KTDatatableHtmlTableDemo = function() {
 			},
 			columns: [
 				{
-					field: 'DepositPaid',
-					type: 'number',
-				},
-				{
-					field: 'OrderDate',
-					type: 'date',
-					format: 'YYYY-MM-DD',
-				}, {
-					field: 'Status',
-					title: 'Status',
+					field: 'Statut',
+					title: 'Statut',
 					autoHide: false,
 					// callback function support for column rendering
-					template: function(row) {
-						var status = {
+					template: function (row) {
+						var statut = {
 							1: {
-                                'title': 'Pending',
-                                'class': ' label-light-warning'
-                            },
+								'title': 'Validée',
+								'class': ' label-light-success'
+							},
 							2: {
-                                'title': 'Delivered',
-                                'class': ' label-light-danger'
-                            },
+								'title': 'Non-validée',
+								'class': ' label-light-warning'
+							},
 							3: {
-                                'title': 'Canceled',
-                                'class': ' label-light-primary'
-                            },
-							4: {
-                                'title': 'Success',
-                                'class': ' label-light-success'
-                            },
-							5: {
-                                'title': 'Info',
-                                'class': ' label-light-info'
-                            },
-							6: {
-                                'title': 'Danger',
-                                'class': ' label-light-danger'
-                            },
-							7: {
-                                'title': 'Warning',
-                                'class': ' label-light-warning'
-                            }
+								'title': 'Annulée',
+								'class': ' label-light-danger'
+							}
 						};
-						return '<span class="label font-weight-bold label-lg' + status[row.Status].class + ' label-inline">' + status[row.Status].title + '</span>';
+						return '<span class="label font-weight-bold label-lg' + statut[row.Statut].class + ' label-inline">' + statut[row.Statut].title + '</span>';
 					},
 				}, {
-					field: 'Type',
-					title: 'Type',
+					field: 'Actions',
+					title: 'Actions',
+					sortable: false,
+					width: 150,
+					overflow: 'visible',
 					autoHide: false,
-					// callback function support for column rendering
-					template: function(row) {
-						var status = {
-							1: {
-                                'title': 'Online',
-                                'state': 'danger'
-                            },
-							2: {
-                                'title': 'Retail',
-                                'state': 'primary'
-                            },
-							3: {
-                                'title': 'Direct',
-                                'state': 'success'
-                            },
-						};
-						return '<span class="label label-' + status[row.Type].state + ' label-dot mr-2"></span><span class="font-weight-bold text-' +status[row.Type].state + '">' +	status[row.Type].title + '</span>';
-					},
-				},
+				}
 			],
 		});
 
+		$('#kt_datatable_search_statut').on('change', function () {
+			datatable.search($(this).val().toLowerCase(), 'Statut');
+		});
 
+		$('#kt_datatable_search_statut').selectpicker();
 
-        $('#kt_datatable_search_status').on('change', function() {
-            datatable.search($(this).val().toLowerCase(), 'Status');
-        });
+	};
 
-        $('#kt_datatable_search_type').on('change', function() {
-            datatable.search($(this).val().toLowerCase(), 'Type');
-        });
-
-        $('#kt_datatable_search_status, #kt_datatable_search_type').selectpicker();
-
-    };
-
-    return {
-        // Public functions
-        init: function() {
-            // init dmeo
-            demo();
-        },
-    };
+	return {
+		// Public functions
+		init: function () {
+			// init dmeo
+			demo();
+		},
+	};
 }();
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 	KTDatatableHtmlTableDemo.init();
 });
--- Desktop/AppProximus/src/Controller/OfferController.php
+++ Desktop/AppProximus/src/Controller/OfferController.php
@@ -3,19 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Agent;
+use App\Entity\Client;
 use App\Entity\Offre;
 use App\Form\AgentType;
 use App\Form\CreationOffreType;
 use App\Form\OffreType;
 use App\Repository\AgentRepository;
+use App\Repository\CategorieRepository;
 use App\Repository\ClientRepository;
 use App\Repository\OffreRepository;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 
 /**
  * @Route("/offres")
@@ -34,16 +43,17 @@ class OfferController extends AbstractController
     /**
      * @Route("/nouvelle/{id}", name="offer_new", methods={"GET", "POST"})
      */
-    public function new(ClientRepository $clientRepository, Request $request, int $id = 0): Response
+    public function new(ClientRepository $clientRepository, ProduitRepository $produitRepository, CategorieRepository $categorieRepository, Request $request, int $id = 0): Response
     {
         $offre = new Offre();
         $offre->setClient($clientRepository->find($id));
+        $offre->setAgent($this->getUser());
+        $categories = $categorieRepository->findAll();
+        //  $produits = $produitRepository->findAll();
+
         $form = $this->createForm(CreationOffreType::class, $offre);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($request);
-
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($offre);
             $entityManager->flush();
@@ -53,48 +63,54 @@ class OfferController extends AbstractController
 
         return $this->render('offer/new.html.twig', [
             'offre' => $offre,
+            'categories' => $categories,
+            //  'produits' => $produits,
             'form' => $form->createView(),
         ]);
     }
 
-    /**
-     * @Route("/nouvelle/{id}", name="offer_new_sale", methods={"GET", "POST"})
-     */
-    public function newSale(ClientRepository $clientRepository, Request $request, int $id = 0): Response
-    {
-        /*
-        $offre = new Offre();
-        $offre->setClient($clientRepository->find($id));
-        $form = $this->createForm(CreationOffreType::class, $offre);
-        $form->handleRequest($request);
+    // /**
+    //  * @Route("/nouvelle/{id}", name="offer_new_sale", methods={"GET", "POST"})
+    //  */
+    // public function newSale(ClientRepository $clientRepository, Request $request, int $id = 0): Response
+    // {
+    //     /*
+    //     $offre = new Offre();
+    //     $offre->setClient($clientRepository->find($id));
+    //     $form = $this->createForm(CreationOffreType::class, $offre);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            dd($request);
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($offre);
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         dd($request);
 
-            return $this->redirectToRoute('offer_index');
-        }
-        */
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($offre);
+    //         $entityManager->flush();
 
-        return $this->render('offer/new.html.twig', [
-            //  'offre' => $offre,
-            //  'form' => $form->createView(),
-        ]);
-    }
+    //         return $this->redirectToRoute('offer_index');
+    //     }
+    //     */
+
+    //     return $this->render('offer/new.html.twig', [
+    //         //  'offre' => $offre,
+    //         //  'form' => $form->createView(),
+    //     ]);
+    // }
 
     /**
      * @Route("/modifier/{id}", name="offer_edit", methods={"GET"})
      */
-    public function edit(Request $request, int $id = 0): Response
+    public function edit(ClientRepository $clientRepository, ProduitRepository $produitRepository, CategorieRepository $categorieRepository, Request $request, int $id = 0): Response
     {
-        /*
         $offre = new Offre();
-        $form = $this->createForm(OffreType::class, $offre);
+        $offre->setClient($clientRepository->find($id));
+        $offre->setAgent($this->getUser());
+        $categories = $categorieRepository->findAll();
+
+        $form = $this->createForm(CreationOffreType::class, $offre);
         $form->handleRequest($request);
 
+        /*
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($offre);
@@ -105,8 +121,134 @@ class OfferController extends AbstractController
         */
 
         return $this->render('offer/edit.html.twig', [
-            //  'offre' => $offre,
-            //  'form' => $form->createView(),
+            'offre' => $offre,
+            'categories' => $categories,
+            //  'produits' => $produits,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/excel/{id}", name="excel", methods={"GET","POST"})
+     */
+    public function GenererExcel(Request $request, Client $client): Response
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        //couleur colonne
+        $evenRow = [
+            'fill' => [
+                'fillType' => Fill::FILL_SOLID,
+                'startColor' => [
+                    'rgb' => 'a6a6a6'
+                ]
+            ]
+        ];
+
+        // set font par defaut
+        $spreadsheet->getDefaultStyle()
+            ->getFont()
+            ->setName('Arial')
+            ->setSize(12);
+
+        $sheet->setCellValue('A1', 'Descriptif');
+        $sheet->setCellValue('B1', '');
+        // set font style de l'entete
+        $spreadsheet->getActiveSheet()->getStyle('A1')->getFont()->setSize(20);
+        // set cell alignment
+        $spreadsheet->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+        //setting column width
+        $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(70);
+        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(70);
+
+        dd($client);
+        // remplissage des cellules
+        $sheet->setCellValue('A2', "Nom du client");
+        $sheet->setCellValue('B2', $client->getClient()->getNom());
+        $sheet->setCellValue('A3', "
+            
+            Prénom du client");
+        $sheet->setCellValue('B3', $client->getPrenom());
+        $sheet->setCellValue('A4', "Date de naissance");
+        $sheet->setCellValue('B4', $client->getDateNaissance());
+        $sheet->setCellValue('A5', "Nom de la société");
+        $sheet->setCellValue('B5', $client->getNomSociete());
+        $sheet->setCellValue('A6', "N° de TVA");
+        $sheet->setCellValue('B6', $client->getNumTVA());
+        $sheet->setCellValue('A7', "Date de Création STE");
+        $sheet->setCellValue('B7', $client->getDateCreationSTE());
+        $sheet->setCellValue('A8', "N° de client Proximus (si existant)");
+        $sheet->setCellValue('B8', $client->getNumClientProximus());
+        $sheet->setCellValue('A9', "Numéro de téléphone fixe");
+        $sheet->setCellValue('B9', $client->getNumTelephoneFixe());
+        $sheet->setCellValue('A10', "Numéro de N° de GSM");
+        $sheet->setCellValue('B10', $client->getNumGSM());
+        $sheet->setCellValue('A11', "Adresse mail");
+        $sheet->setCellValue('B11', $client->getEmail());
+        $sheet->setCellValue('A12', "num IBAN ( si JO)");
+        $sheet->setCellValue('B12', $client->getNumIBAN());
+        $sheet->setCellValue('A13', "Code postal");
+        $sheet->setCellValue('B13', $client->getCodePostal());
+        $sheet->setCellValue('A14', "Commune");
+        $sheet->setCellValue('B14', $client->getCommune());
+        $sheet->setCellValue('A15', "Nom de rue, numéro de porte");
+        $sheet->setCellValue('B15', $client->getRue() . ' ,' . $client->getNumPorte());
+        $sheet->setCellValue('A16', "Si elle est différent, adresse d'installation des services");
+        $sheet->setCellValue('B16', $client->getAdresseInstallation());
+        $sheet->setCellValue('A17', "Produit(s) existant(s) Proximus");
+        $sheet->setCellValue('B17', '');
+        $sheet->setCellValue('A18', "Produit(s) à la concurrence");
+        $sheet->setCellValue('B18', '');
+        $sheet->setCellValue('A19', "Produit(s) vendu(s)");
+        $sheet->setCellValue('B19', '');
+        $sheet->setCellValue('A20', "Prix Plein et Prix Promos");
+        $sheet->setCellValue('B20', '');
+        $sheet->setCellValue('A21', "Dates d'installation souhaitées (3)");
+        $sheet->setCellValue('B21', $client->getDateInstallation());
+        $sheet->setCellValue('A22', "Information pour l’encodage");
+        $sheet->setCellValue('B22', $client->getInfoEncodage());
+        $sheet->setCellValue('A23', "N° de téléphone (fixe et/ou mobile) à porter vers Proximus");
+        $sheet->setCellValue('B23', $client->getNumTelephoneProximus());
+        $sheet->setCellValue('A24', "N° de client chez la concurrence");
+        $sheet->setCellValue('B24', $client->getNumClientConcurrence());
+        $sheet->setCellValue('A25', "N° d'easy switch");
+        $sheet->setCellValue('B25', $client->getNumEasySwitch());
+        $sheet->setCellValue('A26', "N° de carte sim du/des numéro(s) de GSM à porter (seulement si carte prépayée)");
+        $sheet->setCellValue('B26', $client->getNumCarteSIMPrepayee());
+        $sheet->setCellValue('A27', "nom client mobile concurrence et code client");
+        $sheet->setCellValue('B27', $client->getNomClientMobile());
+        $sheet->setCellValue('A28', "choix de l'app pour le mobile (Facebook, etc)");
+        $sheet->setCellValue('B28', $client->getAppMobile());
+        $sheet->setCellValue('A29', "Bonus TV / bouquet");
+        $sheet->setCellValue('B29', $client->getBonusTV());
+        $sheet->setCellValue('A30', "lien partageable de l'enregistrement (drive)");
+        $sheet->setCellValue('B30', $client->getLienDrivePartageable());
+        $sheet->setCellValue('A31', "Carte Identité");
+        $sheet->setCellValue('B31', $client->getCarteIdentite());
+        $sheet->setCellValue('A32', "Commentaire");
+        $sheet->setCellValue('B32', '');
+
+        // application de la couleur
+        $spreadsheet->getActiveSheet()->getStyle('A1:A32')->applyFromArray($evenRow);
+        //border
+        $spreadsheet->getActiveSheet()->getStyle('A1:B32')->getBorders()->applyFromArray(
+            [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                    'color' => [
+                        'rgb' => '000000'
+                    ]
+                ]
+            ]
+        );
+        // creation
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('Fiche descriptive de ' . $client->getPrenom() . ' ' . $client->getNom() . '.xlsx');
+
+        return $this->render('client/show.html.twig', [
+            'client' => $client,
         ]);
     }
 }
